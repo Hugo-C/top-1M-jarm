2dcbf5209250465169aa31ce18e569624e714fb0
Hugo-C <Hugo-C@users.noreply.github.com>
2022-12-21 23:26:22 +0100

    Bump rust_jarm to set timeout

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+use std::time::Duration;
 use pyo3::prelude::*;
 use rust_jarm::Jarm;
 
 #[pyfunction]
 fn compute_jarm_hash(host: String, host_port: Option<String>) -> PyResult<String> {
     let port = host_port.unwrap_or("443".to_string());
-    let result = Jarm::new(host, port).hash();
+    let mut jarm_scan = Jarm::new(host, port);
+    jarm_scan.timeout = Duration::from_secs(2);
+    let result = jarm_scan.hash();
     let hash = result.unwrap_or("00000000000000000000000000000000000000000000000000000000000000".to_string());
     Ok(hash)
 }
